881a394150d24fd50c3b418048dee1fd7565fe9b
yoo16 <yohei.yoshikawa@gmail.com>
2022-07-19 13:44:38 +0900

    update

--- src/main/java/com/example/board/repository/Post.java
+++ src/main/java/com/example/board/repository/Post.java
@@ -6,6 +6,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -18,20 +21,27 @@ import lombok.Data;
 public class Post {
 
     /** ID */
+	@NotNull
     @Id
     @Column
     private String id = null;
 
     /** 投稿者 */
+	@NotEmpty
     @Column(length = 20, nullable = false)
+	@Size(min = 1, max = 20)
     private String author = null;
 
     /** タイトル */
+	@NotEmpty
     @Column(length = 20, nullable = false)
+	@Size(min = 1, max = 20)
     private String title = null;
 
     /** 内容 */
+	@NotEmpty
     @Column(length = 1000, nullable = false)
+	@Size(min = 1, max = 1000)
     private String body = null;
 
     /** 登録日時 */
